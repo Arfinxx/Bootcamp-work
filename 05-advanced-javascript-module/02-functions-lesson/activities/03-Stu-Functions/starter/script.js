13e217d05a8184c6211bbe036f8e7d722d1a19a2
Arfinxx <arfin@hotmail.co.uk>
2024-01-09 20:09:51 +0000

    added module 6 files and completed lesson 2

--- 05-advanced-javascript-module/02-functions-lesson/activities/03-Stu-Functions/starter/script.js
+++ 05-advanced-javascript-module/02-functions-lesson/activities/03-Stu-Functions/starter/script.js
@@ -1,1 +1,30 @@
 // Write Your Code Below
+
+function adds() {
+
+    var sum = 1+2;
+    console.log(sum);
+    return sum;
+};
+
+adds();
+
+var subtracts = function() {
+
+    var minus = 20 - 10;
+    console.log(minus);
+    return minus;
+};
+
+subtracts();
+
+var sideLength = 10;
+
+function area() {
+
+    var squareArea = sideLength * sideLength;
+    console.log(squareArea);
+    return squareArea;
+};
+
+area();
--- 05-advanced-javascript-module/02-functions-lesson/activities/05-Stu-Function-Logic/starter/script.js
+++ 05-advanced-javascript-module/02-functions-lesson/activities/05-Stu-Function-Logic/starter/script.js
@@ -1,1 +1,19 @@
 // Write Your Code Below
+
+function comp(x,y){
+
+    if (x===y){
+        console.log(x + " and " + y + " are equal in value and type.")
+    } else if (x==y){
+
+        console.log(x + " and " + y + " are equal in value only.");
+    } else{
+        console.log(x + " and " + y + " are not equal in any way.");
+    }
+};
+
+comp(2, 2);
+
+comp(2,"2");
+
+comp(2, true);
